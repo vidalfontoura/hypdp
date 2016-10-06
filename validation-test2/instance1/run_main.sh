f28e84afbbbc6fcd9db813652740d3f42a960f8b
vdfontoura <vdfontoura@inf.ufpr.br>
2016-10-06 17:09:57 -0300

    Adding the results

---
+++ validation-test2/instance1/run_main.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+#for i in $(seq 1 11)
+#do
+	#Seeds 1 to 30	
+	for j in $(seq 1 30)
+	do
+		qsub run_hypdp_cluster.sh $j 1 600000 "RC * Ccurrent * Cava - Cr" "( ( TI / Delta ) / ( ( Delta * ( ( TI / Delta ) / CI ) * Delta / Delta * TI ) - CI ) )"
+
+	done
+#done
