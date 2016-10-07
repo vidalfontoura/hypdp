237e63b3e5f7ffa1ec4f3c1a8a586bab4bb58b31
Vidal Daniel <vfontoura@kenzan.com>
2016-10-07 10:29:31 -0300

    Adding fiels to execute experiment

---
+++ validation-test3/instance11/run_main.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+#for i in $(seq 1 11)
+#do
+	#Seeds 1 to 30	
+	for j in $(seq 1 30)
+	do
+		qsub run_hypdp_cluster.sh $j 11 600000 "( ( ( ( ( Caccept / RC ) * Cr / Caccept ) / RC * Cr ) / Caccept / RC ) * Cr ) / Caccept" "( ( ( ( ( CI / PF ) * Delta / CI ) / PF * Delta ) / CI / PF ) * Delta ) / CI"
+
+	done
+#done
